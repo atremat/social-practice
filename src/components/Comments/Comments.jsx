c95f3a1f353786fcc4f43ba7dc874b5267d6a48b
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-05-11 16:19:52 +0300

    task2

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Comment } from "../Comment/Comment";
 import { Grid } from "../Grid/Grid";
-import { comments } from "../../helpers/comments";
+// import { comments } from "../../helpers/comments";
 import { selectFilter } from "../../redux/filterSlice";
 import { useSelector } from "react-redux";
+import { useGetCommentsQuery } from "../../redux/commentApi";
+import { Loader } from "../Loader/Loader"
 
 export const Comments = () => {
   const filterValue = useSelector(selectFilter);
+  const {data:comments, isLoading} = useGetCommentsQuery();
 
-  const filteredComments = comments.filter((comment) =>
+  const filteredComments = comments?.filter((comment) =>
     comment.content.toLowerCase().includes(filterValue.toLowerCase().trim())
   );
   return (
     <Grid>
+      {isLoading && <Loader/>}
       {comments &&
         filteredComments.map((comment) => (
           <Comment key={comment.id} {...comment} />
--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_ENDPOINT = "/comments";
-const BASE_URL = "https://662e4c31a7dda1fa378c947d.mockapi.io/";
+const BASE_URL = "https://662e4c31a7dda1fa378c947d.mockapi.io";
 
 export const commentApi = createApi({
   reducerPath: "comments",
@@ -9,7 +9,33 @@ export const commentApi = createApi({
     baseUrl: BASE_URL,
   }),
   tagTypes: ["Comments"],
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+      providesTags: ["Comments"],
+    }),
+    addComment: builder.mutation({
+      query(comment) {
+        return {
+          url: API_ENDPOINT,
+          method: "POST",
+          body: comment,
+        };
+      },
+      invalidatesTags: ["Comments"],
+    }),
+    updateCommentCount: builder.mutation({
+      query({id, ...data}) {
+        return {
+          url: `${API_ENDPOINT}/${id}`,
+          method: "PUT",
+          body: data,
+        };
+      },
+      invalidatesTags: ["Comments"],
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery, useAddCommentMutation, useUpdateCommentCountMutation } = commentApi;
+
